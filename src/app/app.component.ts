69d412b74082963d0a3d7b21f2311fc68c6625ff
Raymond Kim <ray.kim09@gmail.com>
2017-10-11 15:08:28 -0400

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  time;
+  on = false;
+  on1 = false;
+  on2 = false;
+  on3 = false;
+  on4 = false;
+  click(btn) {
+    if (btn === 'btn1') {
+      this.on = true;
+      this.on1 = true;
+      this.on2 = false;
+      this.on3 = false;
+      this.on4 = false;
+      this.time = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles'});
+    }
+    else if (btn === 'btn2') {
+      this.on = true;
+      this.on1 = false;
+      this.on2 = true;
+      this.on3 = false;
+      this.on4 = false;
+      this.time = new Date().toLocaleString('en-US', { timeZone: 'America/Denver'});
+    }
+    else if (btn === 'btn3') {
+      this.on = true;
+      this.on1 = false;
+      this.on2 = false;
+      this.on3 = true;
+      this.on4 = false;
+      this.time = new Date().toLocaleString('en-US', { timeZone: 'America/Chicago'});
+    }
+    else if (btn === 'btn4') {
+      this.on = true;
+      this.on1 = false;
+      this.on2 = false;
+      this.on3 = false;
+      this.on4 = true;
+      this.time = new Date().toLocaleString('en-US', { timeZone: 'America/New_York'});
+    }
+    else {
+      this.on = false;
+      this.on1 = false;
+      this.on2 = false;
+      this.on3 = false;
+      this.on4 = false;
+    }
+  }
 }
